dc58a335ea5db882bd4287eef884f9ed8abb3877
Christian Anderson <tehninjawaffle@gmail.com>
2024-07-19 13:09:15 -0700

    Installed a new module called fontaine. Seems to generate values and warn you when you have sudden page transitions... or something like that. Well see how it goes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,21 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     }
   },
+
   runtimeConfig: {
     // This is an exposed data that can be grabbed serverside. The most obvious use is providing a key for authentication purposes. Maybe, a secret, as well
     // Another use could be derived from .env variables. Like, defined an api key or changing the project to production, rather than development.
     public: {
       // This is an exposed data that can be grabbed from clientside. Can be used to defined path names like :/api/
     }
-  }
+  },
+
+  modules: ["@nuxtjs/fontaine"]
 })
