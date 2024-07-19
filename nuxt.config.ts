cc298277f49e57008c4d2604b14e769d29d53a93
Christian Anderson <tehninjawaffle@gmail.com>
2024-07-19 12:43:32 -0700

    Added assets, installed tailwind, modifed the nuxt config and tailwind config to work together, added some thoughts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  },
   runtimeConfig: {
     // This is an exposed data that can be grabbed serverside. The most obvious use is providing a key for authentication purposes. Maybe, a secret, as well
     // Another use could be derived from .env variables. Like, defined an api key or changing the project to production, rather than development.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.vue",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
