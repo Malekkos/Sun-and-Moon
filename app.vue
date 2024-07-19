db79e26079bb6d011973aabf526393e09c82541a
Christian Anderson <tehninjawaffle@gmail.com>
2024-07-19 12:14:53 -0700

    Setup placeholders, mocked up what components will be with some thoguhts

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
+
+
+
+<!--
+This is the default entry point for the project. It will render its content on every route of the application
+What needs to be here:
+ ~ Header/Navbar
+ ~ Footer
+
+-->
+
+
 <template>
   <div>
+    <div>
+      <p>Placeholder Navbar</p>
+    </div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <div>
+      <p>Placeholder Footer</p>
+    </div>
   </div>
 </template>
---
+++ components/Book-Card.vue
@@ -0,0 +1,29 @@
+
+<!-- 
+Components are... well, you know what they are.
+One difference is they are available a different way. Rather than defining and importing the components,
+cont. you will directly reference the name of THIS file. AKA: <Book-Card>
+Build how you usually do. In this instance, this will be the repeated card for each new book added to the website.
+Data consumed:
+ ~ Book name
+ ~ Book desc
+ ~ Book Picture
+Constaints:
+ ~ Expandable on click, not hover. Heard somewhere that its better User experience to have the option to click, rather than hover.
+-->
+
+
+
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
