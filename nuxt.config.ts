f32fa0ae6f7601295f4b56dc6fadb73018325e26
Christian Anderson <tehninjawaffle@gmail.com>
2024-07-19 11:57:39 -0700

    Added my initial design file, modified a bit of the config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    // This is an exposed data that can be grabbed serverside. The most obvious use is providing a key for authentication purposes. Maybe, a secret, as well
+    // Another use could be derived from .env variables. Like, defined an api key or changing the project to production, rather than development.
+    public: {
+      // This is an exposed data that can be grabbed from clientside. Can be used to defined path names like :/api/
+    }
+  }
 })
