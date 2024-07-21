aaf2844bf0f8440fd8d2565e3bb138e450cdf5f4
Christian Anderson <tehninjawaffle@gmail.com>
2024-07-21 11:27:25 -0700

    Added the first dynamic route, as well as thoughts. issues are highlighted, well see how we remedy it

---
+++ pages/books/book.vue
@@ -0,0 +1,27 @@
+<!-- 
+ 
+This should be the template page for the dynamic route system.
+It would function(at least, as of now) by taking the param id/book name, passing that info
+cont. to the database and searching it from there. This would be in the form of a call. Then,
+cont. it would print out all the data here.
+Issues:
+ ~ First off, the one that comes to mind immediately, is the data passed. What I mean is not 
+ cont. the normal data itself; name, description, chapters, what have you. More specifically, 
+ cont. we would need pages/chapters that would link off of this page, and that could exceed a thousand.
+ cont. This means we would have a dynamic route inside of a dynamic route. Not sure how that will work.
+
+-->
+
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
