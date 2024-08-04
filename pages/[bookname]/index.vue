6e07ec0e4df3f056827161717790fe2c602c5b13
Christian Anderson <tehninjawaffle@gmail.com>
2024-08-04 16:11:22 -0700

    Added thoughts on about section for books. Dyhnamic routing will be interesting

--- pages/[bookname]/index.vue
+++ pages/[bookname]/index.vue
@@ -1,3 +1,15 @@
+<!-- 
+This will be the about section of a particular book
+Can include Name, picture of book, Author, tags(maybe), and a directory
+cont. to access all the chapters this book has available.
+
+Getting into more complicated things like view count, rating, comments
+cont. can be saved for WAY later.
+-->
+
+
+
+
 <template>
   <div>
     {{ $route.params.bookname }}
