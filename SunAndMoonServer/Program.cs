db77d846107ea40924b07396425df8df230c07ff
Christian Anderson <tehninjawaffle@gmail.com>
2024-09-05 13:38:49 -0700

    Registered the Database Context. Means it should be visible to the controller as BookList

--- SunAndMoonServer/Program.cs
+++ SunAndMoonServer/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore
+using Book.Models
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BookContext>(opt =>
+    opt.UseInMemoryDatabase("BookList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
