27afd4b3f73342055dacb798157955dc23325feb
Christian Anderson <tehninjawaffle@gmail.com>
2024-10-11 10:36:40 -0700

    Okay, so, shockingly, it kinda worked. pgadmin registered the call i was trying to make. But, it errored. And on someting that makes no sense to me. Similarily, the sql thats being registered makes even less sense, at least on the values line. Its registering , incrementing for each field to be inserted into

--- SunAndMoonServer/obj/Debug/net8.0/SunAndMoonServer.AssemblyInfo.cs
+++ SunAndMoonServer/obj/Debug/net8.0/SunAndMoonServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SunAndMoonServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18db0f1eee1e99be8a14bea29565d58105dfd04a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59450bce423b183570ebb8b738d7b89d57197f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SunAndMoonServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SunAndMoonServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
